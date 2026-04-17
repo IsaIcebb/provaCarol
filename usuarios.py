e6c7f504b8d7e88daa7a0b9accf5fb973a3d4643
IsaIcebb <167563308+IsaIcebb@users.noreply.github.com>
2026-04-17 13:50:23 -0300

    Update usuarios.py

--- usuarios.py
+++ usuarios.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 import os
 
-app = Flask(-__name__)
+app = Flask(__name__)
 
 usuarios = [
     {"id": 1, "usuarios": "Miriam"},
@@ -30,6 +30,6 @@ def home():
 def listar_usuarios():
     return jsonify(usuarios)
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT" , 5000))
-    app.run(host="0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
